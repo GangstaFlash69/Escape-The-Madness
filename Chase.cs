82513d5d7f45c9905c1554169292859a7f99ffd2
GangstaFlash <107011242+GangstaFlash69@users.noreply.github.com>
2022-06-14 04:17:10 +0200

    Add files via upload

---
+++ Chase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    public Transform player;
+    public UnityEngine.AI.NavMeshAgent agent;
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+    void OnBecameInvisible()
+    {
+        transform.LookAt(player);
+        Debug.Log("Nigga ain't looking...");
+        gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = false;
+        agent.SetDestination(player.position);
+    }
+     void OnBecameVisible()
+     {
+        gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+        Debug.Log("Stopping...");
+     }
+}
